8eacd00241cbf5ae0e5b218528e1b2276440f8d4
Killian Ca <killiancarty@gmail.com>
2025-03-04 20:02:21 +0000

    first commit

---
+++ src/pages/HomePage.tsx
@@ -0,0 +1,11 @@
+function HomePage() {
+    return (
+      <div>
+        <h1>Welcome to the Library Room Booking System</h1>
+        <p>This is the homepage.</p>
+      </div>
+    );
+  }
+  
+  export default HomePage;
+  
