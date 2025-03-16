639c2ed7a2d49ce44e6e1fa22bb8768ea35bc588
Killian Ca <killiancarty@gmail.com>
2025-03-16 16:00:18 +0000

    deployment fix

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,7 @@
-import { defineConfig } from 'vite'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  base: "/roomManagmentFrontEnd/", // Ensure it matches your repository name
+  plugins: [react()],
+  base: '/roomManagmentFrontEnd/',
 });
